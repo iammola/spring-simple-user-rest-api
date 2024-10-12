3c5e3ce4984c03983904c282983f020b35e5fc8b
Ademola Adedeji <a.mola.dev@gmail.com>
2024-10-12 15:27:39 -0300

    🎉 init: start project from sping initializer

---
+++ src/main/java/com/iammola/spring_simple_user_rest_api/SpringSimpleUserRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.iammola.spring_simple_user_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSimpleUserRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSimpleUserRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iammola/spring_simple_user_rest_api/SpringSimpleUserRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iammola.spring_simple_user_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSimpleUserRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
